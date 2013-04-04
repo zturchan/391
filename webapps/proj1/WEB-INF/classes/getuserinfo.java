1340912adfbf4e4aba6d1a3920a2c7642d68a42f
zturchan <zturchan@ualberta.ca>
2013-04-04 14:06:30 -0600

    fixed what appeared to be a merge error in home.html and fixed indentation in my java files

--- webapps/proj1/WEB-INF/classes/getuserinfo.java
+++ webapps/proj1/WEB-INF/classes/getuserinfo.java
@@ -1,3 +1,7 @@
+/*
+This class provides the class, first name, last name, address, email, phone to the modification form. 
+These are used to pre-populate an html form for user editing. 
+*/
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
@@ -6,59 +10,58 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class getuserinfo extends HttpServlet {
-	public void doGet(HttpServletRequest req, HttpServletResponse res) 
-		throws IOException, ServletException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		res.setContentType("text/html");
 		//establish the connection to the underlying database
-	Connection conn = null;
-	String driverName = "oracle.jdbc.driver.OracleDriver";
-	String dbstring = "jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
-	HttpSession session = req.getSession();
-	String sessionUser = (String) session.getAttribute("userName");
-	PrintWriter out = res.getWriter();
-	try{
-		//load and register the driver	
-		Class drvClass = Class.forName(driverName);
-		DriverManager.registerDriver((Driver) drvClass.newInstance());
-	}
-	catch(Exception ex){
-		out.println("<hr>" + ex.getMessage() + "<hr>");
-	}
-	try{
-		//establish the connection
-		conn = DriverManager.getConnection(dbstring,"zturchan","Pikachu1");
-	    conn.setAutoCommit(false);
-	}
-	catch(Exception ex){
-		out.println("<hr>" + ex.getMessage() + "<hr>");
-	}
-	Statement stmt = null;
-	ResultSet rset = null;
-	String first = "";
-	String last = "";
-	String address = "";
-	String email = "";
-	String phone = "";
-	String sqlinfo = "select first_name,last_name,address,email,phone from persons where USER_NAME = '"+sessionUser+"'";
-	String sqluser = "select class from users where USER_NAME = '"+sessionUser+"'";
-	try{
-		stmt = conn.createStatement();
-		rset = stmt.executeQuery(sqlinfo);
-		while(rset.next()){
-		first = rset.getString(1);
-		last = rset.getString(2);
-		address = rset.getString(3);
-		email = rset.getString(4);
-		phone = rset.getString(5);
+		Connection conn = null;
+		String driverName = "oracle.jdbc.driver.OracleDriver";
+		String dbstring = "jdbc:oracle:thin:@gwynne.cs.ualberta.ca:1521:CRS";
+		HttpSession session = req.getSession();
+		String sessionUser = (String) session.getAttribute("userName");
+		PrintWriter out = res.getWriter();
+		try{
+			//load and register the driver	
+			Class drvClass = Class.forName(driverName);
+			DriverManager.registerDriver((Driver) drvClass.newInstance());
 		}
-		stmt.close();
-		conn.close();
-	}
-	catch(Exception ex){
-		javax.swing.JOptionPane.showMessageDialog(null, "There was an error connecting to the database.  Please ensure you have entered a valid username");
-		out.println("<hr>" + ex.getMessage() + "<hr>");
-	}
-	
+		catch(Exception ex){
+			out.println("<hr>" + ex.getMessage() + "<hr>");
+		}
+		try{
+			//establish the connection
+			conn = DriverManager.getConnection(dbstring,"zturchan","Pikachu1");
+		    conn.setAutoCommit(false);
+		}
+		catch(Exception ex){
+			out.println("<hr>" + ex.getMessage() + "<hr>");
+		}
+		Statement stmt = null;
+		ResultSet rset = null;
+		String first = "";
+		String last = "";
+		String address = "";
+		String email = "";
+		String phone = "";
+		String sqlinfo = "select first_name,last_name,address,email,phone from persons where USER_NAME = '"+sessionUser+"'";
+		String sqluser = "select class from users where USER_NAME = '"+sessionUser+"'";
+		try{
+			stmt = conn.createStatement();
+			rset = stmt.executeQuery(sqlinfo);
+			while(rset.next()){
+				first = rset.getString(1);
+				last = rset.getString(2);
+				address = rset.getString(3);
+				email = rset.getString(4);
+				phone = rset.getString(5);
+			}
+			stmt.close();
+			conn.close();
+		}
+		catch(Exception ex){
+			javax.swing.JOptionPane.showMessageDialog(null, "There was an error connecting to the database.  Please ensure you have entered a valid username");
+			out.println("<hr>" + ex.getMessage() + "<hr>");
+			
+		}
 	res.sendRedirect(		"userinfo.jsp?user="+sessionUser+
 							"&first="+first+
 							"&last="+last+
